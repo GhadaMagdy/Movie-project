4bda38de39b4ec29860463bfb1f404f1947818ff
Ghada Magdy <ghadamagdy93@gmail.com>
2018-06-18 08:41:57 +0200

    changes avter submission review

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,25 +2,25 @@
 from media import Movie
 import fresh_tomatoes
 # create movie instances
-TOY_STORY_POSTER = "https://m.media-amazon.com/images/I/618kQqoT2FL._SL500_.jpg"
-TOY_STORY_TRAILER = "https://www.youtube.com/watch?v=rNk1Wi8SvNc"
-TOY_STORY = Movie.Movie("Toy story", TOY_STORY_POSTER, TOY_STORY_TRAILER)
-AVATAR_POSTER = "https://cdn.traileraddict.com/content/20th-century-fox/avatar-6.jpg"
-AVATAR_TRAILER = "https://www.youtube.com/watch?v=5PSNL1qE6VY"
-AVATAR = Movie.Movie("Avatar", AVATAR_POSTER, AVATAR_TRAILER)
-MAMA_COCO_POSTER = "https://ohmy.disney.com/wp-content/uploads/2017/09/Coco_Payoff_IG_Jpeg_v7-1.jpg"
-MAMA_COCO_TRAILER = "https://www.youtube.com/watch?v=5sSMRg1X1vg"
-MAMA_COCO = Movie.Movie("Mama coco", MAMA_COCO_POSTER, MAMA_COCO_TRAILER)
-RATAOUILLE_POSTER = "https://images-na.ssl-images-amazon.com/images/I/71VDlRubWtL._SL1000_.jpg"
-RATAOUILLE_TRAILER = "https://www.youtube.com/watch?v=e8GBfNo3IHY"
-RATATOUILLE = Movie.Movie("Ratatouille", RATAOUILLE_POSTER, RATAOUILLE_TRAILER)
-MIDNIGHT_POSTER = "https://setinparis.com/wp-content/uploads/2014/05/Midnight-in-Paris.jpg"
-MIDNIGHT_TRAILER = "https://www.youtube.com/watch?v=x_XMltmF0IM"
-MIDNIGHT_IN_PARIS = Movie.Movie("Midnight in paris", MIDNIGHT_POSTER, MIDNIGHT_TRAILER)
-HUNGER_POSTER = "http://i.ebayimg.com/00/s/NTAwWDMzNw==/z/~IkAAOxy7nNTVkmT/$_3.JPG?set_id=2"
-HUNGER_TRAILER = "https://www.youtube.com/watch?v=FovFG3N_RSU"
-HUNGER_GAMES = Movie.Movie("Hunger games", HUNGER_POSTER, HUNGER_TRAILER)
+TOY_IMG = "https://m.media-amazon.com/images/I/618kQqoT2FL._SL500_.jpg"
+TOY_URL = "https://www.youtube.com/watch?v=rNk1Wi8SvNc"
+TOY = Movie.Movie("Toy story", TOY_IMG, TOY_URL)
+AVATAR_IMG = "http://www.impawards.com/2009/posters/avatar_ver2.jpg"
+AVATAR_URL = "https://www.youtube.com/watch?v=5PSNL1qE6VY"
+AVATAR = Movie.Movie("Avatar", AVATAR_IMG, AVATAR_URL)
+MAMA_COCO_IMG = "http://www.impawards.com/2017/posters/coco.jpg"
+MAMA_COCO_URL = "https://www.youtube.com/watch?v=5sSMRg1X1vg"
+MAMACOCO = Movie.Movie("Mama coco", MAMA_COCO_IMG, MAMA_COCO_URL)
+RATAOUILLE_IMG = "http://www.impawards.com/2007/posters/ratatouille.jpg"
+RATAOUILLE_URL = "https://www.youtube.com/watch?v=e8GBfNo3IHY"
+RATATOUILLE = Movie.Movie("Ratatouille", RATAOUILLE_IMG, RATAOUILLE_URL)
+MID_IMG = "http://www.impawards.com/2011/posters/midnight_in_paris.jpg"
+MID_URL = "https://www.youtube.com/watch?v=x_XMltmF0IM"
+MIDNIGHT = Movie.Movie("Midnight in paris", MID_IMG, MID_URL)
+HUNGER_IMG = "http://www.impawards.com/2012/posters/hunger_games.jpg"
+HUNGER_URL = "https://www.youtube.com/watch?v=FovFG3N_RSU"
+HUNGER = Movie.Movie("Hunger games", HUNGER_IMG, HUNGER_URL)
 # create movies list
-MOVIES_LIST = [TOY_STORY, AVATAR, MAMA_COCO, RATATOUILLE, MIDNIGHT_IN_PARIS, HUNGER_GAMES]
+MOVIES_LIST = [TOY, AVATAR, MAMACOCO, RATATOUILLE, MIDNIGHT, HUNGER]
 # call open movie page function to render our page with movies list
 fresh_tomatoes.open_movies_page(MOVIES_LIST)
--- media/Movie.py
+++ media/Movie.py
@@ -1,14 +1,11 @@
 """This module descripes movie variables and methods"""
 import webbrowser
-class Movie:
+class Movie():
     """this class provides way to store movies"""
-    # when you define a constant you should write in camel case
-    VALID_RATING = ["G", "PG", "PG-13", "R"]
-
     def __init__(self, movie_title, movie_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = movie_image
         self.trailer_youtube_url = trailer_youtube
-
     def show_trailer(self):
+        """show trailer method opens the trailer of mvie on youtupe"""
         webbrowser.open(self.trailer_youtube_url)
